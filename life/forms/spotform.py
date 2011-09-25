79e28c7cfd6afea1b85fec768a253d44bf81b5fb
wwq0327 <wwq0327@gmail.com>
2011-09-25 23:52:45 +0800

    select field

--- life/forms/spotform.py
+++ life/forms/spotform.py
@@ -12,13 +12,20 @@
     :license: GPLv3
 """
 
-from flaskext.wtf import Form, TextField, TextAreaField, SubmitField, required, FloatField, FileField
+from flaskext.wtf import Form, TextField, TextAreaField, SubmitField, required, FloatField, FileField, SelectField
 
 class SpotForm(Form):
 
     spot_name = TextField(u"景点名称", validators=[required(message=u"景点名称")])
     spot_loc = TextField(u"景点位置")
-    better_season = TextField(u"理想出行季节")
+    #better_season = TextField(u"理想出行季节")
+    better_season = SelectField(u"理想出行季节", choices=[
+        (u'春季', u'春季'),
+        (u'夏季', u'夏季'),
+        (u'秋季', u'秋季'),
+        (u'冬季', u'冬季'),
+        (u'四季皆可', u'四季皆可'),
+        ], coerce=unicode)
     tickets = FloatField(u"票价")
     content = TextAreaField(u"景点介绍")
     spot_logo = FileField(u"图片")
